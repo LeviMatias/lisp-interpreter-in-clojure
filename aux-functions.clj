da8cf1ca60887512af3faebed31fcc59d2ae950f
LeviMatias <matilevi35@gmail.com>
2020-07-06 01:20:27 -0300

    igual? controlar-aridad revisar-lae

--- aux-functions.clj
+++ aux-functions.clj
@@ -12,18 +12,49 @@
    reduce (fn [a b](or a b)) (in-pairs amb #(= elem (first %)))
 ))
 
+(defn is-nil? [a](
+    or (nil? a) (= a 'NIL) (= a "NIL") (and (coll? a) (= (count a) '0))
+))
+
 ;;;;;;;;;;;;;;
 
-(defn igual? [a, b])(
-
+(defn igual? [a, b](
+    cond (= a b)
+        true
+    (and (is-nil? a) (is-nil? b) )
+        true
+    (= (clojure.string/lower-case (str a)) (clojure.string/lower-case (str b)) )
+        true
+    true
+        false
+)
 )
 
+(defn controlar-aridad [lis val-esperado](
+    cond (> (count lis) val-esperado)
+        ((list '*error* 'too-many-args)
+    (< (count lis) val-esperado)
+        ((list '*error* 'too-few-args))
+    true
+        val-esperado
+))
+
 (defn revisar-f[lis](
-    cond (coll-and-error?)
+    cond (coll-and-error? lis)
         lis
     true
-        
+        nil
+))
 
+(defn revisar-lae[lis](
+    (reduce (fn [a b] (
+        cond (coll-and-error? a)
+            a
+        (coll-and-error? b)
+            b
+        true
+            nil
+    )) lis)
 ))
 
 (defn buscar [elem amb](
