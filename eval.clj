01a1d16f66d680fd9880acd0d6caca2275f776a4
LeviMatias <matilevi35@gmail.com>
2020-07-13 17:47:46 -0300

    cond bug fixes

--- eval.clj
+++ eval.clj
@@ -2,7 +2,7 @@
 (declare evaluar)
 
 (defn evaluar-secuencia-en-cond [lis amb-global amb-local](
-   cond (<= 1 (count list)) (evaluar (first lis) amb-global amb-local)
+   cond (<= 1 (count lis)) (evaluar (first lis) amb-global amb-local)
    true (let [res (evaluar (first lis) amb-global amb-local)](
        if (revisar-f (first res)) res
        (evaluar-secuencia-en-cond (rest lis) (second res) amb-local) 
@@ -11,6 +11,6 @@
 
 (defn evaluar-cond [lis amb-global amb-local](
     cond (is-nil? lis) (list nil amb-global)
-    (evaluar (ffirst lis) amb-global amb-local) (evaluar-secuencia-en-cond (next (ffirst lis)) amb-global amb-local)
+    (evaluar (ffirst lis) amb-global amb-local) (evaluar-secuencia-en-cond (nfirst lis) amb-global amb-local)
     true (evaluar-cond (rest lis) amb-global amb-local)
 ))
