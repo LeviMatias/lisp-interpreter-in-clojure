c8867e9f556af6e446e19b5680bcb1046f9f879f
LeviMatias <matilevi35@gmail.com>
2020-07-13 05:17:41 -0300

    evaluar secuencia en cond

--- eval.clj
+++ eval.clj
@@ -1,8 +1,12 @@
-
+(load-file "control.clj")
 (declare evaluar)
 
 (defn evaluar-secuencia-en-cond [lis amb-global amb-local](
-   cond (= 1 (count list)) (list)
+   cond (<= 1 (count list)) (evaluar (first lis) amb-global amb-local)
+   true (let [res (evaluar (first lis) amb-global amb-local)](
+       if (revisar-f (first res)) res
+       (evaluar-secuencia-en-cond (rest lis) (second res) amb-local) 
+   ))
 ))
 
 (defn evaluar-cond [lis amb-global amb-local](
